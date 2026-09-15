3ee4fa4cb9d68a8c7c075ee2860e8fa685ba958e
Evan Flint <transcendentalguy@gmail.com>
2026-09-15 16:15:43 -0400

    hopefully complete CI

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ def dev3():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8111, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8111, log_level="info")
